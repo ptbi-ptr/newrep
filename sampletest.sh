5f6464be25edbb3d74d7809544df94ac4cb0c6d5
ptbi-ptr <131666392+ptbi-ptr@users.noreply.github.com>
2023-08-03 17:04:48 +0530

    Add files via upload

---
+++ sampletest.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl --insecure --user e4703befb6e9496247981274:3112cfe2b20068b59743b0d8c3c49fa338367c48c41825273942e1c6d4b96d6aa9a07bf8 -F 'files[]=@sample.csv' https://a.blazemeter.com/api/v4/folders/64c2746a315855087d0d73c4/files
+curl --insecure --user e4703befb6e9496247981274:3112cfe2b20068b59743b0d8c3c49fa338367c48c41825273942e1c6d4b96d6aa9a07bf8 -F 'files[]=@latest.csv' https://a.blazemeter.com/api/v4/folders/64c2746a315855087d0d73c4/files
